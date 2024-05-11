3765e1b20d64106bcd3e608d08bfc39bd7e85920
JosueRivera <josue.gabriel.rivera1@gmail.com>
2024-05-11 10:32:05 -0600

    commit3

--- src/pruebapráctica1_josuerivera/PruebaPráctica1_JosueRivera.java
+++ src/pruebapráctica1_josuerivera/PruebaPráctica1_JosueRivera.java
@@ -3,20 +3,46 @@ import java.util.Scanner;
 
 public class PruebaPráctica1_JosueRivera {
     
-    public static void Repetir() {
-        Scanner read = new Scanner(System.in);
+    public static void Repetir() { //método para el Ejercicio de repetir mi nombre
+        Scanner read = new Scanner(System.in); //el escaner a utilizar
         int repeticion;
         System.out.println("Cantidad de veces que se repetirá el texto: ");
         repeticion = read.nextInt();
-        for (int i = 1; i <= repeticion; i++) {
+        for (int i = 1; i <= repeticion; i++) { //ciclo for que ira repitiendo el mensaje hasta que i llegue a la cantidad de veces que el usuario indico
             System.out.println("¡Hola mi nombre es Josué Rivera y miren mi prueba!");
+        }
+        System.out.println(); //se imprime una linea para dejar un espacion entre la oracion y el menu principal
+    }
+    
+    public static void SubmenúDeMensajes() { //método del submenu de mensajes
+        Scanner read = new Scanner(System.in); //el escaner a utilizar
+        int eleccion = 0; //es la variable que se usa como opcion
+        while (eleccion != 3) {
+            System.out.println("1. Bienvenida ");
+            System.out.println("2. Despedida ");
+            System.out.println("3. Salir del submenú ");
+            eleccion = read.nextInt();
+            switch (eleccion) {
+                case 1 -> { //Mensaje de bienvenida
+                    System.out.println("Bienvenido al Submenú, espero tenga un excelente día");
+                }
+                case 2 -> { //Mensajes de despedida
+                    System.out.println("Hasta luego, gracias por usar mi programa");
+                }
+                case 3 -> { //Mensaje que hace saber al usuario que salio del submenu
+                    System.out.println("Usted ha salido del Submenu");
+                }
+                default -> { //si el usuario eligio un numero que no esta dentro de las opciones, este mensaje se lo hara saber, y lo devolvera al submenu
+                    System.out.println("Usted no ha ingresado una opción válida, lo devolveremos al Submenú principal. ");
+                }
+            }
         }
     }
     
-    public static void MenuPrincipal() {
-        Scanner read = new Scanner(System.in);
-        int opcion = 1;
-        int contador = 0;
+    public static void MenuPrincipal() { //método para mostrar el Menu Principal
+        Scanner read = new Scanner(System.in); //el escaner a utilizar
+        int opcion = 1; //variable usada para que el usuario 
+        int contador = 0; //variable utilizada para saber cuantas veces ingreso el usuario al menu principal
         while (opcion != 3) {
             contador++;
             System.out.println("Bienvenido a mi primera prueba práctica en Java, las opciones son las siguientes:");
@@ -26,21 +52,24 @@ public class PruebaPráctica1_JosueRivera {
             opcion = read.nextInt();
             switch (opcion) {
                 case 1 -> {
-                    Repetir();
+                    Repetir(); //llama al metodo Repetir
                 }
                 case 2 -> {
-                    
+                    SubmenúDeMensajes(); //llama al metodo de submenu
                 }
                 case 3 -> {
                     System.out.println("Adiós, está fue mi prueba");
-                    System.out.println("Cantidad de veces que se repitío el menú principal: " + contador);
+                    System.out.println("Cantidad de veces que se repitío el menú principal: " + contador); //este mensaje muestra las veces que se ingreso al menu principal
+                }
+                default -> { //caso en el que el usuario no haya elegido una opcion que este presente en el menu principal
+                    System.out.println("Usted no ha ingresado ninguna opción válida, lo devolverémos al Menú Principal. ");
                 }
             }
         }
     }
     
     public static void main(String[] args) {
-        MenuPrincipal();
+        MenuPrincipal(); //llama al menu principal
     }
     
 }
